fc0b96610a5bfceffd2b995d2811ceac43c9cf0e
Michał Wrzosek <michal@wrzosek.pl>
2019-08-26 23:01:53 +0200

    Updates

---
+++ precache-manifest.9a0dbc228198ac7ea52d23459b05d342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c96278f9542c8be25591b8d2542209a",
+    "url": "/xfor-text-field/index.html"
+  },
+  {
+    "revision": "7bf8683b1b043d2c0baf",
+    "url": "/xfor-text-field/static/css/main.ca7384a5.chunk.css"
+  },
+  {
+    "revision": "ad95f43c802a46dd0a2d",
+    "url": "/xfor-text-field/static/js/2.fd9d62ca.chunk.js"
+  },
+  {
+    "revision": "7bf8683b1b043d2c0baf",
+    "url": "/xfor-text-field/static/js/main.b4287457.chunk.js"
+  },
+  {
+    "revision": "d4531cf8bac871f8236b",
+    "url": "/xfor-text-field/static/js/runtime~main.95532b4e.js"
+  }
+]);
